529223e528ed4244e3613deb227b4f7bac32f8bc
hrishikesh-duvva <hrishikeshduvva@gmail.com>
2024-11-28 09:40:07 +0530

    Initial commit with project files

--- obj/Debug/net8.0/CarRentalSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/CarRentalSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarRentalSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31dd2abb5e65e68894fa773674fd1abb323bbfb2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50573e2e79eaefe02ec909c035b2913451db9c99")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarRentalSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarRentalSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
